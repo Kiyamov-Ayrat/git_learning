2ee676cd5907ea4ddd955018ddb0ebc0ce44cd2f
ayrat <kiamovajrat1@gmail.com>
2025-08-02 22:10:00 +0500

    add file string.py and delete text

--- nums.py
+++ nums.py
@@ -8,6 +8,13 @@ def minus(a, b):
 def nul(a, b):
     return a * b
 
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+def c(n):
+    for i in n:
+        return i
+
 if __name__ == '__main__':
     print(plus(2, 3))
     print(minus(2, 3))
